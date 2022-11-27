d748865ab6d7b2f66ca13a39a7d06f2fdd7a82c3
plosnak2 <jakub.zaukolec@gmail.com>
2022-11-27 17:57:01 +0100

    test

--- proj3.py
+++ proj3.py
@@ -5,11 +5,13 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
+
 # function to extract html document from given url
 def getDocument(url):
     # request for Html document from given URL
     response = requests.get(url)
     return response.text
+
 
 # my URL
 myUrl = "https://store.flightsim.com/product-category/msfs/msfs-scenery/page/2/"
@@ -26,15 +28,17 @@ for i in range(3, 17):
     soup = BeautifulSoup(document, 'html.parser')
 
     # find correct data
-    sceneriesData = soup.find("body").find('div', {'id': 'page-container'}).find('div', {'id': 'left-area'}).find('ul', {'class': 'products columns-3'})
-    
+    sceneriesData = soup.find("body").find('div', {'id': 'page-container'}).find(
+        'div', {'id': 'left-area'}).find('ul', {'class': 'products columns-3'})
+
     # find all sceneries from site, attribute starting with "https://"
-    for newUrl in sceneriesData.find_all('a', attrs={'href': re.compile("^https://")}): 
+    for newUrl in sceneriesData.find_all('a', attrs={'href': re.compile("^https://")}):
         # save url to textfile
         f.write(newUrl.get('href') + '\n')
-        
+
     # change URL to next page for new data
-    myUrl = "https://store.flightsim.com/product-category/msfs/msfs-scenery/page/" + str(i) + '/'
+    myUrl = "https://store.flightsim.com/product-category/msfs/msfs-scenery/page/" + \
+        str(i) + '/'
 
 # close file
 f.close()
---
+++ urls.py
@@ -0,0 +1,44 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+import fileinput
+import sys
+
+
+# function for controlling arguments (limited arguments says i will extract only 20 products)
+def argCheck():
+    arguments = sys.argv[1:]
+    if ("--limited") in arguments:
+        return True
+    else:
+        return False
+
+
+# function to extract html document from given url
+def getDocument(url):
+    # request for Html document from given URL
+    response = requests.get(url)
+    return response.text
+
+
+# open file for writing
+f = open("data.tsv", "w")
+
+limited = argCheck()
+for line in fileinput.input(encoding="utf-8"):
+    myUrl = line.replace('\n', '')
+    # create document
+    document = getDocument(myUrl)
+    # create soap object
+    soup = BeautifulSoup(document, 'html.parser')
+    # selceting name of product
+    title = soup.find(class_='product_title entry-title').getText()
+    # selecting price of product
+    price = soup.find(class_='price').getText()
+    # constructing final string
+    final_string = myUrl + "\t" + title + "\t" + price
+    # save url to textfile
+    f.write(final_string + '\n')
+
+# close file
+f.close()
